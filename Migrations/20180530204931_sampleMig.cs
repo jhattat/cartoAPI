ce7a999c95a5558a1d0b6e0b7ec82aea71d57547
jhattat <jhattat@agilead.fr>
2018-05-30 23:20:01 +0200

    Add initialisation and special Geoloc update

---
+++ Migrations/20180530204931_sampleMig.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace obsApi.Migrations
+{
+    public partial class sampleMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AZObsItems",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    IsComplete = table.Column<bool>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AZObsItems", x => x.Id);
+                });
+                 //migrationBuilder.Sql(@"ALTER TABLE [dbo].[AZObsItems] ADD [Location] geography NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AZObsItems");
+        }
+    }
+}
---
+++ Migrations/20180530211244_ajoutGeoloc.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace obsApi.Migrations
+{
+    public partial class ajoutGeoloc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"ALTER TABLE [dbo].[AZObsItems] ADD [Location] geography NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
