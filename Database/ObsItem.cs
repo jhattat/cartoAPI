42e956f2fbc4b1e0b7596a70902ab38f4c299171
jhattat <jhattat@agilead.fr>
2018-05-30 23:19:24 +0200

    removed unused stuff

--- Database/ObsItem.cs
+++
@@ -1,9 +0,0 @@
-namespace ObsApi.Models
-{
-    public class ObsItem
-    {
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public bool IsComplete { get; set; }
-    }
-}
